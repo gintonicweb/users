21635093aa5722540644e851cbb26e300f942301
Philippe Lafrance <phil.laf@gmail.com>
2015-09-01 02:07:06 -0400

    Tests setup now updated with a test app

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,33 +1,74 @@
 <?php
-// @codingStandardsIgnoreFile
+
 use Cake\Cache\Cache;
 use Cake\Core\Configure;
 use Cake\Core\Plugin;
 use Cake\Datasource\ConnectionManager;
 use Cake\I18n\I18n;
+use Cake\Log\Log;
+use Cake\Routing\Router;
+
 require_once 'vendor/autoload.php';
 
-// Path constants to a few helpful things.
 if (!defined('DS')) {
     define('DS', DIRECTORY_SEPARATOR);
 }
-define('ROOT', dirname(__DIR__) . DS);
-define('CAKE_CORE_INCLUDE_PATH', ROOT . 'vendor' . DS . 'cakephp' . DS . 'cakephp');
-define('CORE_PATH', ROOT . 'vendor' . DS . 'cakephp' . DS . 'cakephp' . DS);
-define('CAKE', CORE_PATH . 'src' . DS);
-define('TESTS', ROOT . 'tests');
-define('APP', ROOT . 'tests' . DS);
-define('APP_DIR', 'app');
-define('WEBROOT_DIR', 'webroot');
-define('WWW_ROOT', dirname(APP) . DS . 'webroot' . DS);
+
+define('ROOT', dirname(__DIR__));
+define('APP_DIR', 'src');
+
 define('TMP', sys_get_temp_dir() . DS);
-define('CONFIG', ROOT . 'tests' . DS . 'config' . DS);
-define('CACHE', TMP);
-define('LOGS', TMP);
+define('LOGS', TMP . 'logs' . DS);
+define('CACHE', TMP . 'cache' . DS);
+define('SESSIONS', TMP . 'sessions' . DS);
+
+define('CAKE_CORE_INCLUDE_PATH', ROOT . DS . 'vendor' . DS . 'cakephp' . DS . 'cakephp');
+define('CORE_PATH', CAKE_CORE_INCLUDE_PATH . DS);
+define('CAKE', CORE_PATH . 'src' . DS);
+define('TEST_APP', ROOT . DS . 'tests' . DS . 'test_app' . DS);
+
+// Point app constants to the test app.
+define('APP', TEST_APP . APP_DIR . DS);
+define('WWW_ROOT', TEST_APP . 'webroot' . DS);
+define('CONFIG', TEST_APP . 'config' . DS);
+
+$loader = new \Cake\Core\ClassLoader;
+$loader->register();
+$loader->addNamespace('App', APP);
+
+//@codingStandardsIgnoreStart
+@mkdir(LOGS);
+@mkdir(SESSIONS);
+@mkdir(CACHE);
+@mkdir(CACHE . 'views');
+@mkdir(CACHE . 'models');
+//@codingStandardsIgnoreEnd
+
+require_once CORE_PATH . 'config/bootstrap.php';
 
 date_default_timezone_set('UTC');
 mb_internal_encoding('UTF-8');
+
 Configure::write('debug', true);
+Configure::write('App', [
+    'namespace' => 'App',
+    'encoding' => 'UTF-8',
+    'base' => false,
+    'baseUrl' => false,
+    'dir' => APP_DIR,
+    'webroot' => 'webroot',
+    'wwwRoot' => WWW_ROOT,
+    'fullBaseUrl' => 'http://localhost',
+    'imageBaseUrl' => 'img/',
+    'jsBaseUrl' => 'js/',
+    'cssBaseUrl' => 'css/',
+    'paths' => [
+        'plugins' => [TEST_APP . 'Plugin' . DS],
+        'templates' => [APP . 'Template' . DS],
+        'locales' => [APP . 'Locale' . DS],
+    ]
+]);
+
 Cache::config([
     '_cake_core_' => [
         'engine' => 'File',
@@ -41,16 +82,38 @@ Cache::config([
     ]
 ]);
 
+// Ensure default test connection is defined
 if (!getenv('db_dsn')) {
-    //putenv('db_dsn=postgres://postgres@127.0.0.1/cakephp_test');
     putenv('db_dsn=sqlite:///:memory:');
 }
-Cake\Datasource\ConnectionManager::config('test', [
-    'url' => getenv('db_dsn'),
-    'timezone' => 'UTC'
+
+ConnectionManager::config('test', ['url' => getenv('db_dsn')]);
+ConnectionManager::config('test_custom_i18n_datasource', ['url' => getenv('db_dsn')]);
+
+Configure::write('Session', [
+    'defaults' => 'php'
 ]);
 
-Plugin::load('Users', ['path' => ROOT]);
+Log::config([
+    'debug' => [
+        'engine' => 'Cake\Log\Engine\FileLog',
+        'levels' => ['notice', 'info', 'debug'],
+        'file' => 'debug',
+    ],
+    'error' => [
+        'engine' => 'Cake\Log\Engine\FileLog',
+        'levels' => ['warning', 'error', 'critical', 'alert', 'emergency'],
+        'file' => 'error',
+    ]
+]);
 
+Carbon\Carbon::setTestNow(Carbon\Carbon::now());
+ini_set('intl.default_locale', 'en_US');
+
+Router::reload();
 Cake\Routing\DispatcherFactory::add('Routing');
 Cake\Routing\DispatcherFactory::add('ControllerFactory');
+Plugin::load('Users', [
+    'path' => ROOT . DS,
+    'routes' => true
+]);
