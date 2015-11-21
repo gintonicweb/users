cb3e67c59481f5980ff8f038ae3852f0a873b5ae
Philippe Lafrance <phil.laf@gmail.com>
2015-11-20 23:53:56 -0500

    Bootstrapping tests for API and using 'username' as display field

---
+++ tests/TestCase/Controller/Api/UsersControllerTest.php
@@ -0,0 +1,37 @@
+<?php
+namespace Users\Test\TestCase\Controller\Api;
+
+use Cake\ORM\TableRegistry;
+use Cake\TestSuite\IntegrationTestCase;
+use Users\Controller\Api\UsersController;
+
+/**
+ * Users\Controller\UsersController Test Case
+ */
+class UsersControllerTest extends IntegrationTestCase
+{
+    public $fixtures = [
+        'plugin.users.users'
+    ];
+
+    public function testBeforeFilter()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    public function testIndex()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    public function testAdd()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    public function testToken()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+}
